faf39756e612c71feb4bbbacdfc4b7bf5ea4c610
Lucas Guidotti <lucasguidottipro@gmail.com>
2025-07-28 09:00:14 -0300

    chore: adiciona imagens de logo do artezzana ao repositório

--- app/views/index.php
+++ app/views/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Artezzana - Marketplace para Produtores Rurais e Artesãos</title>
-    <link rel="stylesheet" href="./css/styles.css">
-    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="../../public/css/styles.css">
+    <link rel="stylesheet" href="../../public/css/global.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
